eac420c3b2b26b8794b4efd9dc94788e1922d461
Taylor Rose <trrosebudd@gmail.com>
2025-03-02 16:36:41 -0700

    Favicon and start of [code] dynamic variable in URL path structure

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,6 +4,7 @@ import { Providers } from '@/app/providers'
 import { Layout } from '@/components/Layout'
 
 import '@/styles/tailwind.css'
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata = {
   title: {
