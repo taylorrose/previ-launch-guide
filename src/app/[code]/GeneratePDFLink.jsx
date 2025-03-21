804ecdee04d237732045932f13f6a74775e4c398
Taylor Rose <trrosebudd@gmail.com>
2025-03-20 18:36:07 -0600

    Pre-generated and saved .pdfs

--- src/app/[code]/GeneratePDFLink.jsx
+++ src/app/[code]/GeneratePDFLink.jsx
@@ -6,7 +6,7 @@ import { Button } from '@/components/Button'
 
 export function GeneratePDFLink({ children }) {
   const { code } = useParams()
-  const url = `https://api.urlbox.com/v1/vGsAgTEIiyqn96rI/pdf?url=https%3A%2F%2Fwww.previ.guide%2F${code}%2Fpdf-preview&width=2550&height=3300&pdf_page_size=letter&pdf_background=true&pdf_dpi=300&pdf_margin=default&full_width=true&pdf_auto_crop=true`
+  const url = `../pdf/${code}.pdf`
 
   return (
     <div className="not-prose">
--- src/app/[code]/sms-preview/[template]/route.js
+++ src/app/[code]/sms-preview/[template]/route.js
@@ -99,7 +99,7 @@ export async function GET(request, { params }) {
     <button onclick="copyHTML()">
         <i class="fa-solid fa-copy"></i> Copy Message
     </button>
-           <a href="https://api.urlbox.com/v1/vGsAgTEIiyqn96rI/pdf?url=https%3A%2F%2Fwww.previ.guide%2F${code}%2Fpdf-preview&width=2550&height=3300&pdf_page_size=letter&pdf_background=true&pdf_dpi=300&pdf_margin=default&full_width=true&pdf_auto_crop=true" target="_blank" style="
+           <a href="/pdf/${code}.pdf" target="_blank" style="
         margin-left:40px;
         font-size:16px;
         text-decoration:none;
